87dcab523b1d4c5b8c8c31cbcbf9b9f2a9f76b30
hemanth <lshemanth777.com>
2025-04-27 10:03:17 +0530

    counter function

--- firstvite_proj/src/App.jsx
+++ firstvite_proj/src/App.jsx
@@ -1,5 +1,12 @@
+import Count from "./Count";
+
 function App() {
-  return <h1>welcome to vite + react | LS</h1>;
+  return (
+    <>
+      <h1>welcome to vite + react | LS</h1>;
+      <Count />
+    </>
+  );
 }
 
 export default App;
